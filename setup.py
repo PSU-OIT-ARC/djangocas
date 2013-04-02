76eb1a2c3070a4f64769c4cb9de36006632ad633
Matt Johnson <mdj2@pdx.edu>
2013-04-02 09:36:16 -0700

    django_cas2

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    author='CPCC',
-    author_email='webapps@cpcc.edu',
+    author='mdj2',
+    author_email='mdj2@pdx.edu',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
@@ -22,7 +22,7 @@ setup(
     keywords='django cas cas2 authentication middleware backend',
     license='MIT',
     long_description="""
-``django_cas`` is a `CAS`_ 1.0 and CAS 2.0 authentication backend for
+``django_cas2`` is a `CAS`_ 1.0 and CAS 2.0 authentication backend for
 `Django`_. It allows you to use Django's built-in authentication mechanisms
 and ``User`` model while adding support for CAS.
 
@@ -33,8 +33,8 @@ to the admin interface.
 .. _CAS: http://www.ja-sig.org/products/cas/
 .. _Django: http://www.djangoproject.com/
 """,
-    name='django_cas',
-    packages=['django_cas'],
-    url='http://code.google.com/p/django-cas/',
-    version='2.1.1',
+    name='django_cas2',
+    packages=['django_cas2'],
+    url='https://github.com/mdj2/django-cas',
+    version='1.0.0',
 )
